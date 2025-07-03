0158ae23c31a82d2dbc73d7473e99c2eeda4d5f5
Rahul-252506 <rahulsramya@gmail.com>
2025-07-03 19:16:48 +0530

    Update nithin.js

--- nithin-backend/nithin.js
+++ nithin-backend/nithin.js
@@ -27,7 +27,7 @@ app.post('/extract', async (req, res) => {
     const browser = await puppeteer.launch({
   headless: true,
   args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined
+  
 });
 
     const page = await browser.newPage();
